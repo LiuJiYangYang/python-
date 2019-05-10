921f7d30ae90c9407fc07b7abb51cf356260debf
liujiyang <1318335493@qq.com>
2019-05-10 21:58:24 +0800

    第二课后习题

---
+++ python小游戏/practice/Hello.py
@@ -0,0 +1,6 @@
+hello = input("请输入你的姓名：")
+Hello = str(hello)
+if Hello == "小甲鱼":
+    print("你好，小甲鱼！")
+
+
---
+++ python小游戏/practice/clac.py
@@ -0,0 +1,16 @@
+clac = input("请输入1到100之间的数字：")
+Clac = float(clac)
+if 100 >= Clac >=1:
+    print("你妹好漂亮^_^")
+else:
+    print("你大爷好丑T_T")
+
+
+# BIF 就是 Built-in Functions，内置函数。
+
+
+
+
+
+
+
