6100f17475585299bcc72f467092a601b8d15355
liujiyang <1318335493@qq.com>
2019-05-10 22:27:19 +0800

    second

---
+++ python小游戏/variablestr.py
@@ -0,0 +1,40 @@
+# python 没有“变量”只有“名字"
+
+teacher = "liujiyang"
+print(teacher)
+teacher = "xingren"
+print(teacher)
+# 字符串拼接
+myteacher = 'liujiyang'
+yourteacher = 'tang'
+ourteacher = myteacher + yourteacher
+print(ourteacher)
+str1 = '''
+先赋值，后调用
+变量名包含字母，数字，下划线。不能以数字开头
+python区分大小写
+= 是赋值，不能写反
+尽量取一个专业的名字（英文）
+字符串：也成为文本，用引号表示字符串
+单引号和双引号同时用，转义符
+'''
+print(str1)
+str='C:\now'
+str='C:\\now'
+print(str)
+# 原始字符串最后不能加反斜杠
+# 长字符串    三重引号字符串
+'''
+Web应用开发
+系统网络运维
+科学与数字计算
+3D游戏开发
+图形界面开发
+网络编程
+'''
+
+
+
+
+
+
