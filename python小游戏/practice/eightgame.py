c9249ddb7e09f3d0138a42933c48ba780cca59ca
liujiyang <1318335493@qq.com>
2019-05-15 22:27:46 +0800

    right game

--- python小游戏/practice/eightgame.py
+++ python小游戏/practice/eightgame.py
@@ -20,5 +20,54 @@ while True:
     break
 print(3)     # 打印出：2     3
 
+# 提供该while效率
+i = 0
+string = 'ILoveFishC.com'
+while i < len(string):
+    print(i)
 
+
+i = 0
+string = 'ILoveFishC.com'
+length = len(string)
+while i < length:
+    print(i)
+    i += 1
+
+count = 3
+password = 'FishC.com'
+
+while count:
+    password = input('请输入密码：')
+    if password == password:
+        print('密码正确，进入程序......')
+        break
+    elif '*' in password:
+        print('密码中不能含有“*”号！你还有',count,'次机会',end='')
+        continue
+    else:
+        print('密码输入错误！你还有',count-1,'次机会！',end='')
+    count -= 1
+
+
+# 如果一个 3 位数等于其各位数字的立方和，则称这个数为水仙花数
+# 153 = 1^3 + 5^3 + 3^3，因此 153 就是一个水仙花数
+for i in range(100, 1000):
+    sum = 0
+    temp = i
+    while temp:
+        sum = sum + (temp%10) ** 3
+        temp //= 10
+    if sum == i:
+        print(i)
+
+
+# 三色求问题
+print('red\tyellow\tgreen')
+for red in range(0,4):
+    for yellow in range(0,4):
+        for green in range(2,7):
+            if red + yellow + green == 8:
+                print(red, '\t', yellow, '\t',green)
+                # 不是字符串拼接，不能用“+”
 
