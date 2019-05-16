a4ac184a1cdb572073d81983eb96341795bb0fc3
liujiyang <1318335493@qq.com>
2019-05-16 22:24:33 +0800

    list

--- python小游戏/list.py
+++ python小游戏/list.py
@@ -14,3 +14,47 @@ member.extend(['呵呵', '来分'])
 
 # insert指定添加
 member.insert(1, ['红花'])
+
+# 获取数组值
+print(member[0])
+
+# 赋值(调换位置)
+temp = member[0]
+member[0] = member[1]
+member[1] = temp
+print(temp)
+
+# 从列表删除元素
+member.remove('怡静')
+print(member)         #  必须知道元素名字
+
+
+# 删除列表
+del member[1]
+# del member
+
+# 从列表中取出最后一个元素并返回给你
+member.pop()
+
+# 把pop取出的参数赋值给name
+name = member.pop()
+print(name)
+
+member.pop(1)
+
+# 列表分片（Slice）
+member[1:3]      # 从索引1开始到3不包含三
+# 列表本身未变
+
+member[:]
+member[1:]
+member[:3]
+
+
+member2 = member[:]
+
+
+
+
+
+
