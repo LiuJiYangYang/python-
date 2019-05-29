cc10fa29d3fad96f33e05ef85d6dea6700bb8a0a
liujiyang <1318335493@qq.com>
2019-05-29 21:33:17 +0800

    recursion 汉诺塔游戏

--- python小游戏/recursion递归.py
+++ python小游戏/recursion递归.py
@@ -53,10 +53,17 @@ if result != -1:
 # 分制思想
 
 
-
-
-
-
+# 汉诺塔游戏，The towers of Hanoi
+# X-->Y-->Z-->X
+def hanoi(n, x, y, z):
+    if n == 1:
+        print(x, "-->", z)
+    else:
+        hanoi(n-1, x, z, y) # 将前n-1个盘子从x移动到y上
+        print(x, "-->", z)# 将最后一个盘子从x移动到z上
+        hanoi(n-1, y, x, z)# 将y上的n-1个盘子移动到z上
+n=int(input("请输入汉诺塔的层数："))
+hanoi(n, 'x', 'y', 'z')
 
 
 
